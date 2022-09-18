32d3ff770a12f21860a59465ec57c10f543c1ce3
TON RUGERIA <tonrugeria@gmail.com>
2022-09-18 08:14:33 +0800

    added sign in and firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sign-in/sign-in.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Shop = () => {
+  return(
+    <div>This is the Shop</div>
+  )
 }
 
-export default App;
+const App = () => {
+  return(
+    <Routes>
+      <Route path='/' element={ <Navigation /> } >
+        <Route index element={ <Home /> } />
+        <Route path='shop' element={ <Shop /> } />
+        <Route path='sign-in' element={ <SignIn /> } />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
